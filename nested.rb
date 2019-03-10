e9e3d19aece72301437a8e1ab66ebce3f35a6b45
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-03-10 21:30:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,16 +57,12 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].join(" ")
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
-	programmer_hash = 
+   programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -79,8 +77,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+	
+	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+   
+   
+   # add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
 end
 
 def changing_alan
@@ -102,6 +106,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+    [:alan_kay][:known_for] = [:alan_kay][:alans_new_info]
+    [:alans_new_info] << "GUI"
      
 end
 
